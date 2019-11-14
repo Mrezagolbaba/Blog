df68dcd605d55d632fb2b73da0e0e9366613c241
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-14 13:54:46 +0330

    Add blog Screen

---
+++ src/blog/Blog.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import Header from "./Header";
+
+export default class Blog extends Component{
+    render(){
+        return (
+            <div>
+                <Header/>
+
+            </div>
+        );
+    }
+
+}
+
