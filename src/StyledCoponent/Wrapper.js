9e0da4d69ab770e651dbd49b9b4f4902526a3f2c
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-14 13:57:47 +0330

    add wrapper for blog header screen

---
+++ src/StyledCoponent/Wrapper.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper= styled.div`
+    grid-column-start:1;
+    display: grid;
+    grid-template-columns:1fr 1fr;
+    grid-template-rows:0.5fr 1fr ;
+    width:100vw;
+    height:450px;
+   
+    color:#fff;  
+`;
+export default Wrapper
+
