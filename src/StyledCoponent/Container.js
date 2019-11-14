8dec7bbc3b0b94da3c0839ee7f7ddc579cc95637
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-14 13:56:21 +0330

    add container style for header blog screen

---
+++ src/StyledCoponent/Container.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const  Container= styled.div`
+    width:100%;
+    display: grid;
+    grid-template-columns: 20% auto 20%; 
+    background: linear-gradient(-30deg, #2e23a6 0%, #c069ce 99%);
+`;
+export default Container
