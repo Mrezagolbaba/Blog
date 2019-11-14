d1adefceff874b68b8c65b6c9a25d84e41773c07
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-14 13:56:53 +0330

    add MenuItem style  for blog header screen

---
+++ src/StyledCoponent/MenuItem.js
@@ -0,0 +1,49 @@
+    import styled from 'styled-components';
+
+    const MenuItem= styled.div`
+        display: flex;
+        font-size: 18px;
+        font-weight: 500;
+        color:#fff;
+        align-Items:center;
+        grid-column-start:2
+        & p{
+            padding:10px 10px;
+            
+        }
+    `;
+    const Button= styled.button`
+       width: 200px;
+       height: 60px;
+       border-radius: 30px;
+       background-color: transparent;
+       border: 2px solid #ffffff;
+       margin-left:20px;
+       align-items: center;
+      justify-content: center;
+      display:flex;
+        & p {
+        font-size: 18px;
+        color: #ffffff;
+        font-family: "Montserrat";
+        font-weight: 500;
+        padding:0;
+        }
+    `;
+    const TitleHeader=styled.div`
+       align-items: center;
+      justify-content: center;
+      display:flex;
+      font-size: 36px;
+      color: #ffffff;
+      font-weight: bold;
+    `;
+
+
+    export {
+        MenuItem,
+        Button,
+        TitleHeader
+
+    }
+
