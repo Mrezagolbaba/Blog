d970579eb80fcbb3f8bc4eae13125d2e3a454d51
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-16 01:28:01 +0330

    add store and route to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React,{Component} from 'react';
-import styled from 'styled-components'
-import Blog from "./blog/Blog";
-const StyledContainer = styled.div({
+import {Provider} from "react-redux";
+import {
+    Route,
+    Switch,
+} from 'react-router-dom';
+import configureStore from '../src/Redux/store/store';
+import MainBlog from "./blog/Blog";
+import SingleBlog from "./blog/blogSingle";
 
-});
 
 
 
 class App extends Component {
   render() {
-
+      const store = configureStore();
     return (
-        <StyledContainer>
-
-          <Blog/>
-        </StyledContainer>
+        <Provider store={store}>
+            <Switch>
+                <Route exact path="/" component={MainBlog}/>
+                <Route path="/singleArticle" component={SingleBlog}/>
+            </Switch>
+        </Provider>
     );
   }
 }
