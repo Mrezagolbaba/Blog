3e301fd7c7c5931acc2cae884cf330b99b8bede8
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-16 01:28:27 +0330

    connect Blog to Redux

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,15 +1,48 @@
 import React,{Component} from 'react';
 import Header from "./Header";
+import ContentBlog from "./ContentBlog";
+import Footer from "./Footer";
+import * as Immutable from 'immutable';
+import { connect } from 'react-redux';
 
-export default class Blog extends Component{
-    render(){
-        return (
-            <div>
-                <Header/>
+import {getArticles} from '../Redux/actions';
+import articleReducers from "../Redux/reducers/articles";
 
-            </div>
-        );
+ class MainBlog extends Component{
+
+    componentDidMount () {
+        this.props.articlesActions()
+
     }
 
+
+
+    render(){
+            return (
+                <div>
+                    <Header/>
+                    <ContentBlog/>
+                    <Footer/>
+                </div>
+            );
+        }
+
+
 }
+function mapStateToProps (state) {
+     console.log(state);
+    return {
+        articles:state.articleReducer
+    }
+}
+// We can dispatch actions to the reducer and sagas
+function mapDispatchToProps (dispatch) {
+    return {
+        articlesActions:()=>{
+            dispatch(getArticles())
+        }
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainBlog);
 
