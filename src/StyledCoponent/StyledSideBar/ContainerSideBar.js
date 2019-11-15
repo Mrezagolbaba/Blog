a8b5bfb838dd74ebe2933b18fc4335fcd5b87e90
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-16 01:16:41 +0330

    create sidebar component

---
+++ src/StyledCoponent/StyledSideBar/ContainerSideBar.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+import {device} from '../Responsive/device';
+
+
+const SideBarContainer=styled.div`
+    grid-column-start:2;
+      margin-top: 150px;
+
+
+`;
+const SearchBarStyled=styled.input`
+  width: 271px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: #ffffff;
+  border: 2px solid #e6e6e6;
+  outline: none;
+  padding: 10px;
+  margin-bottom: 59px;
+   @media ${device.laptop} { 
+       width: 200px;
+      height: 40px;
+   }
+  
+`;
+const CategoriesHolder=styled.div`
+  width: 271px;
+
+`;
+const CategoriesItems=styled.p`
+    font-size: 16px;
+    color: #666666;
+    font-family: "Montserrat";
+    font-weight: 400;
+    border-bottom: 1px solid #e6e6e6;
+    padding-bottom: 5px;
+`;
+const TitleCategoriesItems=styled.p`
+    font-size: 22px;
+    line-height: 60px;
+    color: #293039;
+    font-family: "Montserrat";
+    font-weight: 500;
+
+`;
+const LatestPostsHolder=styled.div`
+  width: 271px;
+  margin-top: 86px;
+
+`;
+const TitleLatestPostsItems=styled.p`
+    font-size: 22px;
+    color: #293039;
+    font-family: "Montserrat";
+    font-weight: 500;
+`;
+const LatestPostsItemsHolder=styled.p`
+   display: flex;
+   //width: 330px;
+   //height: 73px;
+`;
+const LatestPostsItemsName=styled.p`
+    font-size: 16px;
+    color: #666666;
+    font-family: "Montserrat";
+    font-weight: 400;
+    margin-left: 10px;
+     margin-bottom: 0px;!important;
+
+
+`;
+const LatestPostsItemsDate=styled.p`
+    font-size: 14px;
+    letter-spacing: 0px;
+    color: #94a1ac;
+    font-family: "Montserrat";
+    font-weight: 400;
+    margin-bottom: 0px;!important;
+    margin-left: 10px;
+`;
+const LatestPostsTextHolder=styled.p`
+   
+`;
+const LatestPostsItemsImage=styled.img`
+    width: 72px;!important;
+    height: 72px;!important;
+    background-color: #e9eef2;
+`;
+
+export {
+    SideBarContainer,
+    SearchBarStyled,
+    TitleCategoriesItems,
+    CategoriesHolder,
+    CategoriesItems,
+    LatestPostsHolder,
+    TitleLatestPostsItems,
+    LatestPostsItemsName,
+    LatestPostsItemsHolder,
+    LatestPostsItemsDate,
+    LatestPostsTextHolder,
+    LatestPostsItemsImage
+}
