c41909c52b9275452ee915783c30cad970b39692
Mohamadreza <m.reza.golbaba@gmail.com>
2019-11-24 22:00:43 +0330

    add some comment

--- src/Components/Screens/Blog/Blog.js
+++ src/Components/Screens/Blog/Blog.js
@@ -1,13 +1,20 @@
+//this file contain some code for first screen called Blog
+// i create this file with
+// 3 part
+// -ContentBlog _Header -Footer
+
+
 import React,{Component} from 'react';
-import Header from "./Header";
+import Header from "../Header/Header";
 import ContentBlog from "./ContentBlog";
-import Footer from "./Footer";
+import Footer from "../Footer/Footer";
 import { connect } from 'react-redux';
 import Loader from 'react-loader-spinner'
-import {getArticles} from '../../Redux/actions';
+import {getArticles} from '../../../Redux/actions';
 
  class MainBlog extends Component{
 
+//in componentDidMount we can call some action to fetch data from server side
     componentDidMount(){
      this.props.articlesActions()
     }
@@ -38,7 +45,10 @@ import {getArticles} from '../../Redux/actions';
 
 
 }
+
+//we can receive some props from redux
 const mapStateToProps= (state)=> {
+
     return {
         articles:state.articleReducer
     }
@@ -52,6 +62,7 @@ const mapDispatchToProps= (dispatch)=> {
     };
 };
 
+//connect Component to Redux and export this
 const BlogComponent= connect(mapStateToProps, mapDispatchToProps)(MainBlog);
  export default BlogComponent
 
